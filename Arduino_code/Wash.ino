64291a371eb50300e198da544414a52ba76d9493
Kravitz Lab <alexxai@wustl.edu>
2025-12-03 11:51:37 -0600

    Restructure project directories and remove unused files
    Moved Arduino libraries, Bonsai code, and PCB designs to new organized directories. Renamed main lickometer code file. Deleted multiple unused Arduino code modules, header files, and setup documentation to clean up the repository.

--- Arduino_code/Wash.ino
+++
@@ -1,8 +0,0 @@
-void wash() {
-  int volume = 200;
-  dispense(0);
-  dispense(1);
-  dispense(2);
-  dispense(3);
-  dispense(4);
-}
--- Arduino_code/customDelay.ino
+++
@@ -1,7 +0,0 @@
-void customDelay(int milliseconds) {
-  unsigned long startTime = millis(); // get the current time
-  while (millis() - startTime < milliseconds) {
-    // do nothing and wait for the specified time
-  }
-  UpdateDisplay(); //update the OLED display even during delays
-}
